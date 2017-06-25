b17cd7dee55fcdd48376b3e4d7c9685a473a7d02
Priyank Gada <kustom.hackers@gmail.com>
2017-06-25 20:30:27 +0530

    Create HelloWorld_VisualStudio.cs

---
+++ HelloWorld_VisualStudio.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Priyank Gada");
+            Console.ReadLine();
+        }
+    }
+}
+#www.youtube.com/priyankgada
