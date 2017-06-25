b6079212983d24cd54c442bd9ca97f9849dbcfaf
Priyank Gada <kustom.hackers@gmail.com>
2017-06-25 18:52:11 +0530

    Create HelloWorld.cs

---
+++ HelloWorld.cs
@@ -0,0 +1,10 @@
+public class HelloWorld
+{
+	public static void Main()
+	{
+		System.Console.WriteLine("Hello World Priyank Gada"); // Prints Line
+		System.Console.ReadLine(); // Shows Output
+	}
+}
+
+#full Video - priyankgada.com www.youtube.com/priyankgada
