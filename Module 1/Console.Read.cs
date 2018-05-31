8e5738a37bf7439ad93b34da1b9b10bed0984593
Priyank Gada <kustom.hackers@gmail.com>
2018-05-31 18:11:25 +0530

    Create Console.Read.cs

---
+++ Module 1/Console.Read.cs
@@ -0,0 +1,26 @@
+//www.priyankgada.com
+//www.youtube.com/priyankgada
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Using .... will add external files to our program
+
+//Comments ! Wont be executed
+
+//Blue words are keywords reserved for our system
+namespace Chapter1program1simpleconsoleapp
+{
+    class Program
+    {
+        static void Main(string[] args) //Program starts from here 
+        {
+            Console.WriteLine("Hello World ! Priyank Gada  "); //Writes on the screen - text displayed
+            //Console.Read();
+            //Console.ReadLine();
+            //Console.ReadKey();
+        }
+    }
+}
