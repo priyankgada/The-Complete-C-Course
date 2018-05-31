aa50ad460994f2231470714a83bac22f70f8749f
Priyank Gada <kustom.hackers@gmail.com>
2018-05-31 18:24:55 +0530

    Create Comments.cs

---
+++ Module 1/Comments.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//www.priyankgada.com
+//www.youtube.com/priyankgada
+
+
+//Using .... will add external files to our program
+
+//Comments ! Wont be executed
+
+//Blue words are keywords reserved for our system
+namespace Chapter1program1simpleconsoleapp
+{
+    class Program
+    {
+        static void Main(string[] args) //Program starts from here 
+        {
+            Console.WriteLine("Hello World ! Priyank Gada  "); //Statements
+            //statements end with ; semi col;;;;;;;;;
+
+            Console.Read();
+
+            /* This is line one
+             * This is line 2
+             * this is line 3
+             * this is line 4
+             * i need many lines
+             * lot of comments
+             * This is a multiline comment
+             */
+             
+        } //Block of code
+    }
+}
